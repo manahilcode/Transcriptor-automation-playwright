53844ed9bf50fa0b2ac2b4f50b794b9500b16f9b
TECHNIFI <manahiladil137@gmail.com>
2025-07-07 16:25:27 +0500

    final

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',  // Location of your test files
-  //timeout: 30000,      // Max time for each test
+      // Max time for each test
   retries: 1,          // Retry on failure
   use: {
     headless: true,            // Run browser in headless mode
